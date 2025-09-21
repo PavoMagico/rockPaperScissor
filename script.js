96bfe9d1c5e7d7ab308669ff562eeb86c75e4662
PavoMagico <santidpdc@gmail.com>
2025-09-21 11:00:09 +0100

    Script de UI

--- script.js
+++ script.js
@@ -12,3 +12,35 @@ function resultadoRonda(jugador, maquina) {
     if (VENCE_A[jugador] === maquina) return "ganas!";
     return "perdiste!";
 }
+
+// Estado Interfaz de usuario o Marcador
+
+const controles = document.getElementById("controles");
+const elJugador = document.getElementById("jugada-jugador");
+const elMaquina = document.getElementById("resultados");
+const elResultado = document.getElementById("score-jugador");
+const elScoreJ = document.getElementById("score-jugador");
+const elScoreM = document.getElementById("score-maquina");
+const btnReset = document.getElementById("reset");
+
+let scoreJ = 0;
+let scoreM = 0;
+
+function ActualizarUI(jugador, maquina, res) {
+    elJugador.textContent = jugador;
+    elMaquina.textContent = maquina;
+    elResultado.textContent = res;
+
+    if (res === "ganas") scoreJ++;
+    else if (res === "pierdes") scoreM++;
+}
+
+function reiniciar() {
+    scoreJ = 0;
+    scoreM = 0;
+    elScoreJ.textContent = "0";
+    elScoreM.textContent = "0";
+    elJugador.textContent = "-";
+    elMaquina.textContent = "-";
+    elResultado.textContent = "-";
+}
