80ea0fa67203171279a5714495f2156c82439c88
PavoMagico <santidpdc@gmail.com>
2025-09-21 10:48:08 +0100

    Lógica de botones y movimientos

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Hello World!");
+// Lógica
+const MOVES = ["piedra", "papel", "tijera"];
+const VENCE_A = { piedra: "tijera", papel: "piedra", tijera: "papel" };
+
+function jugadaMaquina() {
+    const n = Math.floor(Math.random() * 3); // 0..2
+    return MOVES[n];
+}
