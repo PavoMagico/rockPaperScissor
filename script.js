3dd0208363f6263211b03dd70fced9fdc5ae5d8b
PavoMagico <santidpdc@gmail.com>
2025-09-22 17:59:24 +0100

    Código completo con funciones para cada accion

--- script.js
+++ script.js
@@ -9,16 +9,16 @@ function jugadaMaquina() {
 
 function resultadoRonda(jugador, maquina) {
     if (jugador === maquina) return "empate";
-    if (VENCE_A[jugador] === maquina) return "ganas!";
-    return "perdiste!";
+    if (VENCE_A[jugador] === maquina) return "ganas";
+    return "pierdes";
 }
 
 // Estado Interfaz de usuario o Marcador
 
 const controles = document.getElementById("controles");
 const elJugador = document.getElementById("jugada-jugador");
-const elMaquina = document.getElementById("resultados");
-const elResultado = document.getElementById("score-jugador");
+const elMaquina = document.getElementById("jugada-maquina");
+const elResultado = document.getElementById("resultado");
 const elScoreJ = document.getElementById("score-jugador");
 const elScoreM = document.getElementById("score-maquina");
 const btnReset = document.getElementById("reset");
@@ -26,13 +26,16 @@ const btnReset = document.getElementById("reset");
 let scoreJ = 0;
 let scoreM = 0;
 
-function ActualizarUI(jugador, maquina, res) {
+function actualizarUI(jugador, maquina, res) {
     elJugador.textContent = jugador;
     elMaquina.textContent = maquina;
     elResultado.textContent = res;
 
     if (res === "ganas") scoreJ++;
     else if (res === "pierdes") scoreM++;
+
+    elScoreJ.textContent = String(scoreJ);
+    elScoreM.textContent = String(scoreM);
 }
 
 function reiniciar() {
@@ -44,3 +47,18 @@ function reiniciar() {
     elMaquina.textContent = "-";
     elResultado.textContent = "-";
 }
+
+// EVENT LISTENER BUTTONS
+controles.addEventListener("click", (e) => {
+    const btn = e.target.closest("button");
+    if (!btn || !btn.dataset.move) return;
+
+    const jugador = btn.dataset.move; // para piedra, papel, tijera
+    const maquina = jugadaMaquina();
+    const res = resultadoRonda(jugador, maquina);
+    actualizarUI(jugador, maquina, res);
+});
+
+btnReset.addEventListener("click", reiniciar);
+
+console.log("Juego Listo");
